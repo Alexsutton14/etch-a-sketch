575c4edf3b554bee79a97816a57bfe7d2b1c5557
Alex Sutton <alexsutton14@hotmail.com>
2019-08-17 20:43:18 +0100

    Added reset and resize function

--- logic.js
+++ logic.js
@@ -1,14 +1,32 @@
 const mainContainer = document.getElementById("container");
+const rowsInput = document.getElementById("rows-input");
+const resetButton = document.getElementById("reset-button");
+const maxRows = 100;
 
 function changeColor(cellId){
     cell = document.getElementById(cellId);
     cell.style.backgroundColor = "black";
 }
 
-function buildGrid(rows){
+function buildGrid(){
+    let rows = rowsInput.value;
+    //Ends function if input is not a number
+    if(isNaN(rows)){
+        console.log("Input NaN");
+        return null;
+    }
+    //limits input to maxRows value
+    if (rows > maxRows){
+        rows = maxRows;
+        rowsInput.value = maxRows;
+    }
     //calculate % size to assign cells and rows for even spacing
     let split = 100/rows;
-
+    //removes any children to main container before rebuilding grid
+    var oldRows = document.getElementsByClassName("grid-row");
+    for(let r=0; r < oldRows.length;){
+        oldRows[r].remove();
+    } 
     //creates as many rows as requested
     for(let i=0; i < rows; i++){
         let rowNumber = i;
@@ -31,4 +49,6 @@ function buildGrid(rows){
         mainContainer.appendChild(newDiv);
     }
 }
-buildGrid(16);
+rowsInput.value = 16;
+buildGrid();
+resetButton.addEventListener("click", function(){buildGrid(rowsInput.value)});
